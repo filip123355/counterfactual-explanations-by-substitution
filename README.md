99d3312315714f11194665d76cebb5ff27a8450f
filip123355 <filip@matenko.pl>
2026-04-26 18:21:43 +0200

    Add uv and data loading

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # counterfactual-explanations-by-substitution
 Implementation of Counterfactual Explanations by Substitution, using Region Constrained Schrödinger Bridges (RCSB)
+
+## Setup
+
+To set up the project, firts create the `.env` file by copying the `.env.example` and adjusting the paths to your data. Then install `uv` if you don't have it and run `uv sync` to install the dependencies.
+
+
---
+++ src/constants.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+from decouple import Config, RepositoryEnv
+
+# Data paths
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DOTENV_PATH = PROJECT_ROOT / '.env'
+
+try:
+	config = Config(RepositoryEnv(str(DOTENV_PATH)))
+except Exception as e:
+	print(f"Error loading .env file: {e}")
+	
+CUB_DATASET = config("DATA_PATH")
+
+# Training parameters
+BATCH_SIZE = 64
+
+# Other 
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+
+
+
+
+
+
