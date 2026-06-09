5902ddfc017aeaedf9e0d04eef3ab9b28a7bbae3
filip123355 <filip@matenko.pl>
2026-06-09 19:38:27 +0200

    Run many experiments

---
+++ scripts/run_retrain_for_many.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+for top_k in 1 2 3; do
+    for seed in 42 43 44 45 46; do
+        tmp=$(mktemp /tmp/retrain_XXXX.yaml)
+        sed "s/^SEED: .*/SEED: ${seed}/" configs/retrain.yaml > "$tmp"
+        sed -i "s/^TOP_K: .*/TOP_K: ${top_k}/" "$tmp"
+        python3 -m scripts.run_retrain --config "$tmp"
+        rm "$tmp"
+    done
+done
+
