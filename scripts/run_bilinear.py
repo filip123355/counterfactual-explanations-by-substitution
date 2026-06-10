7d8cdbec8cce4d84bdd1cf89db6b85f0d440370f
filip123355 <filip@matenko.pl>
2026-06-10 19:45:03 +0200

    Refactor bilinear model

--- scripts/run_bilinear.py
+++ scripts/run_bilinear.py
@@ -1,19 +1,12 @@
-import os
 import torch
 import mlflow
-from PIL import Image
 from loguru import logger
-from mlflow.tracking import MlflowClient
 
 from src.shapley import BilinearModel
 from src.data import CelebADataset, CompositeFeature, Feature
 from src.inpainter.guidance.classifier import get_classifier
-from src.substitution import MediapipeFaceKeypointDetector
 from src.utils import load_config, parse_args, log_config_params
 from src.constants import TRACKING_URI
-from src.inpainter.guidance import CLIPGuidance
-from src.inpainter.i2sb import I2SB
-from src.interface.clip import load_clip
 
 FEATURE_MAP = {
     "eyes": CompositeFeature.eyes,
@@ -21,16 +14,6 @@ FEATURE_MAP = {
     "mouth": CompositeFeature.mouth,
     "hair": Feature.hair,
 }
-
-
-def download_shapley_values_artifact(
-    client: MlflowClient,
-    run_id: str,
-    target_idx: int,
-    n: int,
-) -> str:
-    artifact_path = f"shapley/values/target_{target_idx}_{n}_shapley_values.json"
-    return client.download_artifacts(run_id, artifact_path)
 
 
 def main():
@@ -54,42 +37,22 @@ def main():
         mlflow.log_param("device", str(device))
 
         dataset = CelebADataset(split=config["DATASET_SPLIT"])
-        face_keypoint_detector = MediapipeFaceKeypointDetector()
         model = get_classifier().to(device)
         features = [FEATURE_MAP[feature] for feature in config["FEATURES"]]
-        client = MlflowClient(tracking_uri=TRACKING_URI)
-        first_order_values_path = download_shapley_values_artifact(
-            client=client,
-            run_id=config["SHAPLEY_1_RUN_ID"],
-            target_idx=config["TARGET_INDEX"],
-            n=1,
-        )
-        second_order_values_path = download_shapley_values_artifact(
-            client=client,
-            run_id=config["SHAPLEY_2_RUN_ID"],
-            target_idx=config["TARGET_INDEX"],
-            n=2,
-        )
-        guidance = CLIPGuidance(load_clip(device=device))
-        target_hq_idx = dataset.data.iloc[config["TARGET_INDEX"]]["idx"]
-        target_image_path = os.path.join(dataset.img_dir, f"{target_hq_idx}.jpg")
-        guidance.set_target(target_img=Image.open(target_image_path).convert("RGB"))
-        inpainter = I2SB(device=device, guidance=guidance)
+
         bilinear_model = BilinearModel(
             features=features, # ty: ignore
             target_idx=config["TARGET_INDEX"],
-            first_order_values_path=first_order_values_path,
-            second_order_values_path=second_order_values_path,
+            first_order_experiment_name=config["FIRST_ORDER_EXPERIMENT_NAME"],
+            second_order_experiment_name=config["SECOND_ORDER_EXPERIMENT_NAME"],
+            run_name_temp=config["SHAPLEY_RUN_NAME_TEMPLATE"],
+            interaction_level=config.get("INTERACTION_LEVEL", 2),
             dataset=dataset,
-            inpainter=inpainter,
-            face_keypoint_detector=face_keypoint_detector,
         )
         r_squared = bilinear_model.calculate_r_squared(
-            ref_indices=list(range(config["REF_INDICES_RANGE"][0], config["REF_INDICES_RANGE"][1])), 
             model=model, 
             device=device,
-            tau=config["TAU"],
-            nfe=config["NFE"],
+            predict_prob=config["PRED_PROB"],
         )
 
         mlflow.log_metric("r_squared", r_squared)
