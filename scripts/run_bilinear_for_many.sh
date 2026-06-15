34e785d2e04783548e5106425ee661d8d8e3f6fb
filip123355 <filip@matenko.pl>
2026-06-15 20:47:06 +0200

    Add some bilinear experiments

--- scripts/run_bilinear_for_many.sh
+++ scripts/run_bilinear_for_many.sh
@@ -5,7 +5,7 @@ set -e
 for idx in 2471 1586 1275 2646 2712 1050 933 1242 497 2606 1855 429 942 2813 1865 1745 173 1552 2356 2683 2692 622 2217 1258 2189 137 988 1622 2781 447 1909 575 1982 792 2451 2155 1185 386 804 2696; do
     tmp=$(mktemp /tmp/bilinear_XXXX.yaml)
     sed "s/^TARGET_INDEX: .*/TARGET_INDEX: ${idx}/" configs/bilinear.yaml > "$tmp"
-    sed -i "s/^MLFLOW_RUN_NAME: .*/MLFLOW_RUN_NAME: \"final_${idx}_i2sb_tau_0.5_nfe_10\"/" "$tmp"
+    sed -i "s/^MLFLOW_RUN_NAME: .*/MLFLOW_RUN_NAME: \"final_${idx}_blackfill\"/" "$tmp"
     python3 -m scripts.run_bilinear --config "$tmp"
     rm "$tmp"
 done
